45d94efff8b7fa5c33e692f964c7f4dfaba4806b
Abhay778 <199605928+Abhay778@users.noreply.github.com>
2026-03-09 21:35:36 +0530

    Refactor photo URLs to use static links

--- Frontend/src/Components/Gallery/PhotoUrls.js
+++ Frontend/src/Components/Gallery/PhotoUrls.js
@@ -1,85 +1,4 @@
-import g1 from "../../assets/Glimses/Glimpse25/20250404_213921.jpg";
-import g2 from "../../assets/Glimses/Glimpse25/20250406_214134.jpg";
-import g3 from "../../assets/Glimses/Glimpse25/ASU00144.JPG";
-import g4 from "../../assets/Glimses/Glimpse25/DSC00001.JPG";
-import g5 from "../../assets/Glimses/Glimpse25/DSC00006.JPG";
-import g6 from "../../assets/Glimses/Glimpse25/DSC00459.JPG";
-import g7 from "../../assets/Glimses/Glimpse25/DSC00724.JPG";
-import g8 from "../../assets/Glimses/Glimpse25/DSC00754.JPG";
-import g9 from "../../assets/Glimses/Glimpse25/DSC00755.JPG";
-import g10 from "../../assets/Glimses/Glimpse25/IMG_0034.JPG";
-import g11 from "../../assets/Glimses/Glimpse25/IMG_0725.JPG";
-import g12 from "../../assets/Glimses/Glimpse25/IMG_0728.JPG";
-import g13 from "../../assets/Glimses/Glimpse25/IMG_0991.JPG";
-import g14 from "../../assets/Glimses/Glimpse25/IMG_0992.JPG";
-import g15 from "../../assets/Glimses/Glimpse25/IMG_1449.JPG";
-import g16 from "../../assets/Glimses/Glimpse25/IMG_1514.JPG";
-import g17 from "../../assets/Glimses/Glimpse25/IMG_1515.JPG";
-import g18 from "../../assets/Glimses/Glimpse25/IMG_1519.JPG";
-import g19 from "../../assets/Glimses/Glimpse25/IMG_1555.JPG";
-import g21 from "../../assets/Glimses/Glimpse25/IMG_1574.JPG";
-import g22 from "../../assets/Glimses/Glimpse25/IMG_1598.JPG";
-import g23 from "../../assets/Glimses/Glimpse25/IMG_1643.JPG";
-import g24 from "../../assets/Glimses/Glimpse25/IMG_1647.JPG";
-import g26 from "../../assets/Glimses/Glimpse25/IMG_1700.JPG";
-import g28 from "../../assets/Glimses/Glimpse25/IMG_1731.JPG";
-import g30 from "../../assets/Glimses/Glimpse25/IMG_1909.JPG";
-import g31 from "../../assets/Glimses/Glimpse25/IMG_1971.JPG";
-import g32 from "../../assets/Glimses/Glimpse25/IMG_2023.JPG";
-import g34 from "../../assets/Glimses/Glimpse25/IMG_2085.JPG";
-import g35 from "../../assets/Glimses/Glimpse25/IMG_2375.JPG";
-import g36 from "../../assets/Glimses/Glimpse25/IMG_2381.JPG";
-import g37 from "../../assets/Glimses/Glimpse25/IMG_2429.JPG";
-import g38 from "../../assets/Glimses/Glimpse25/IMG_2432.JPG";
-import g39 from "../../assets/Glimses/Glimpse25/IMG_2452.JPG";
-import g40 from "../../assets/Glimses/Glimpse25/IMG_2464.JPG";
-import g41 from "../../assets/Glimses/Glimpse25/IMG_2467.JPG";
-import g42 from "../../assets/Glimses/Glimpse25/IMG_2499.JPG";
-import g43 from "../../assets/Glimses/Glimpse25/IMG_2544.JPG";
-import g44 from "../../assets/Glimses/Glimpse25/IMG_2587.JPG";
-import g45 from "../../assets/Glimses/Glimpse25/IMG_2655.JPG";
-import g46 from "../../assets/Glimses/Glimpse25/IMG_2683.JPG";
-import g47 from "../../assets/Glimses/Glimpse25/IMG_2790.JPG";
-import g48 from "../../assets/Glimses/Glimpse25/IMG_2800.JPG";
-import g49 from "../../assets/Glimses/Glimpse25/IMG_2916.JPG";
-import g50 from "../../assets/Glimses/Glimpse25/IMG_3064.JPG";
-import g51 from "../../assets/Glimses/Glimpse25/IMG_3261.JPG";
-import g52 from "../../assets/Glimses/Glimpse25/IMG_3497.JPG";
-import g53 from "../../assets/Glimses/Glimpse25/IMG_3503.JPG";
-import g54 from "../../assets/Glimses/Glimpse25/IMG_3776.JPG";
-import g55 from "../../assets/Glimses/Glimpse25/IMG_4085.JPG";
-import g56 from "../../assets/Glimses/Glimpse25/IMG_4086.JPG";
-import g57 from "../../assets/Glimses/Glimpse25/IMG_4134.JPG";
-import g58 from "../../assets/Glimses/Glimpse25/IMG_6810.JPG";
-import g59 from "../../assets/Glimses/Glimpse25/IMG_6826.JPG";
-import g60 from "../../assets/Glimses/Glimpse25/IMG_6833.JPG";
-import g61 from "../../assets/Glimses/Glimpse25/IMG_6883.JPG";
-import g62 from "../../assets/Glimses/Glimpse25/IMG_6895.JPG";
-import g63 from "../../assets/Glimses/Glimpse25/IMG_6928.JPG";
-import g64 from "../../assets/Glimses/Glimpse25/IMG_7085.JPG";
-import g65 from "../../assets/Glimses/Glimpse25/IMG_7106.JPG";
-import g66 from "../../assets/Glimses/Glimpse25/IMG_7107.JPG";
-import g67 from "../../assets/Glimses/Glimpse25/IMG_7136.JPG";
-import g69 from "../../assets/Glimses/Glimpse25/PXL_20250404_051925507.MP.jpg";
-import g70 from "../../assets/Glimses/Glimpse25/PXL_20250404_053700054.MP.jpg";
-import g71 from "../../assets/Glimses/Glimpse25/PXL_20250404_053855211.MP.jpg";
-import g72 from "../../assets/Glimses/Glimpse25/PXL_20250404_105412860.MP.jpg";
-import g73 from "../../assets/Glimses/Glimpse25/PXL_20250404_112253974.MP.jpg";
-import g74 from "../../assets/Glimses/Glimpse25/PXL_20250404_160018582.MP.jpg";
-import g75 from "../../assets/Glimses/Glimpse25/ASU00544.JPG"
-
-const glimpse25Photos = [
-  g1, g2, g3, g4, g5, g6, g7, g8, g9, g10,
-  g11, g12, g13, g14, g15, g16, g17, g18, g19, 
-  g21, g22, g23, g24, g26, g28, g30,
-  g31, g32,  g34, g35, g36, g37, g38, g39, g40,
-  g41, g42, g43, g44, g45, g46, g47, g48, g49, g50,
-  g51, g52, g53, g54, g55, g56, g57, g58, g59, g60,
-  g61, g62, g63, g64, g65, g66, g67, g69, g70,
-  g71, g72, g73, g74, g75
-];
-
-const oldPhotoUrls = [
+const photoUrls = [
   "https://i.postimg.cc/5yzV5X5R/37.jpg",
   "https://i.postimg.cc/c4QWr8LK/39.jpg",
   "https://i.postimg.cc/W4fLQGXy/40.jpg",
@@ -129,7 +48,5 @@ const oldPhotoUrls = [
   "https://i.postimg.cc/28nSRt94/9.jpg",
   "https://i.postimg.cc/P548b9Nj/36.jpg",
 ];
-
-const photoUrls = [...glimpse25Photos, ...oldPhotoUrls];
 
 export default photoUrls;
