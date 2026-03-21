7a1808b8e9bf9d5c5d6c948a75c5a3e723d7fe20
Vivek Mani Tripathy <vmtprimary@gmail.com>
2026-03-22 00:49:37 +0530

    Fixing login persistance II

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.VITE_API_BASE_URL ||
-  import.meta.env.VITE_BACKEND_API_URL;
-
 const api = axios.create({
-  baseURL,
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
   withCredentials: true,
 });
 
